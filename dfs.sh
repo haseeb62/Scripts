486781a66a1c8ce3b6453ad783e90e1494f82629
Haseeb-ur-Rehman Faheem <23100044@lums.edu.pk>
2023-02-17 14:34:52 +0500

    dfs slight changes

--- dfs.sh
+++ dfs.sh
@@ -32,10 +32,6 @@ kill_spade(){
 
 
 try_stop_kill_spade(){
-  #if is_spade_running; then
-   # stop_spade
-  ##fi
-  #sleep 5
   if is_spade_running; then
     kill_spade
   fi
@@ -59,15 +55,17 @@ send_spade_command(){
 
 dfs_runner(){
     local uuid = "${1}"
+    local count = "${2}"
     echo "UUID: $uuid"
-    send_spade_command "%$count = \"UUID\"=='$uuid'" #uuid
-    send_spade_command "\$$count = \$base.getVertex(%$count)" #getVertex
-    send_spade_command "\$lineage_$count = \$base.getLineage(\$$count, 5,'d')" #$lineage 
-    send_spade_command "\$vertices_$count = \$lineage_$count.getVertex()" #vertexset
-    send_spade_command "\$parents_$count = \$base.getLineage(\$vertices_$count, 1,'a')" #parents
-    send_spade_command "\$skeleton_$count = \$lineage_$count + \$parents+_$count"  #union
-    send_spade_command "\$subgraph_$count = \$base.getSubgraph(\$skeleton_$count)" #getsubgraph
-    send_spade_command "\$dfs_$count = \$subgraph.transform(temporaltraversalprime)" #transformer
+    echo "count : $count" 
+    # send_spade_command "%$count = \"uuid\"=='$uuid'" #uuid
+    # send_spade_command "\$$count = \$base.getVertex(%$count)" #getVertex
+    # send_spade_command "\$lineage_$count = \$base.getLineage(\$$count, 5,'d')" #$lineage 
+    # send_spade_command "\$vertices_$count = \$lineage_$count.getVertex()" #vertexset
+    # send_spade_command "\$parents_$count = \$base.getNeighbor(\$vertices_$count, 'a')" #parents
+    # send_spade_command "\$skeleton_$count = \$lineage_$count + \$parents_$count"  #union
+    # send_spade_command "\$subgraph_$count = \$base.getSubgraph(\$skeleton_$count)" #getsubgraph
+    # send_spade_command "\$dfs_$count = \$subgraph_$count.transform(TemporalTraversalPrime, \"order=timestamp\", \$$count, 'd')" #transformer
 }
 
 
@@ -87,39 +85,18 @@ uuids="${uuids// /\\n}"
 while IFS= read -r uuid; do # looping over each UUID
     # Print each uuid
     echo "UUID: $uuid"
-    send_spade_command "%$count = \"uuid\"=='$uuid'" #uuid
-    send_spade_command "\$$count = \$base.getVertex(%$count)" #getVertex
-    send_spade_command "\$lineage_$count = \$base.getLineage(\$$count, 5,'d')" #$lineage 
-    send_spade_command "\$vertices_$count = \$lineage_$count.getVertex()" #vertexset
-    send_spade_command "\$parents_$count = \$base.getLineage(\$vertices_$count, 1,'a')" #parents
-    send_spade_command "\$skeleton_$count = \$lineage_$count + \$parents_$count"  #union
-    send_spade_command "\$subgraph_$count = \$base.getSubgraph(\$skeleton_$count)" #getsubgraph
-    send_spade_command "\$dfs_$count = \$subgraph_$count.transform(TemporalTraversalPrime, \"order=timestamp\", \$$count, 'd')" #transformer
+    dfs_runner uuid count
+    # send_spade_command "%$count = \"uuid\"=='$uuid'" #uuid
+    # send_spade_command "\$$count = \$base.getVertex(%$count)" #getVertex
+    # send_spade_command "\$lineage_$count = \$base.getLineage(\$$count, 5,'d')" #$lineage 
+    # send_spade_command "\$vertices_$count = \$lineage_$count.getVertex()" #vertexset
+    # send_spade_command "\$parents_$count = \$base.getNeighbor(\$vertices_$count, 'a')" #parents
+    # send_spade_command "\$skeleton_$count = \$lineage_$count + \$parents_$count"  #union
+    # send_spade_command "\$subgraph_$count = \$base.getSubgraph(\$skeleton_$count)" #getsubgraph
+    # send_spade_command "\$dfs_$count = \$subgraph_$count.transform(TemporalTraversalPrime, \"order=timestamp\", \$$count, 'd')" #transformer
     count=$((count+1))
     #L(V1) then find V(L(V1)) then find P(V(L(V1) then Union P(V(L(V1) with L(V1) and on this union call getSubgraph
 done < <(echo "$uuids")
-
-#   local i= name= cdm_log_path= dot_path= idx=0
-#   local line_count=1
-#   try_stop_kill_spade
-#   clear_spade_cfg
-#   "${spade_bin}" start
-#    sleep 2
-#   send_spade_command "add storage Quickstep"
-#     for i in {114..119}; do
-#     echo "ta1-trace-e3-official.bin.${i}"
-#     send_spade_command "add reporter CDM /home/vagrant/trace_bins/ta1-trace-e3-official.bin.${i}"
-#     while : ; do
-#     sleep 10
-#     value=$( grep "File processing" "/home/vagrant/SPADE/log/current.log" | wc -l )
-#     echo $value
-#      if [ "$value" -eq "${line_count}" ]
-#       then
-#       line_count = $((line_count + 1))
-#       break
-#      fi
-#      done
-#   done
 }
 
 
