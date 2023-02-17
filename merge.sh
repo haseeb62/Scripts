e4f01b390561ee5992e5d336f4f93d91c4ce9c56
Haseeb-ur-Rehman Faheem <23100044@lums.edu.pk>
2023-02-17 20:42:24 +0500

    bug fixed

--- merge.sh
+++ merge.sh
@@ -42,7 +42,7 @@ merge() {
   echo "x"
   echo "y"
   send_spade_command "\$intersect = \$${subgraph}_$x &  \$${subgraph}_$y"
-  local z="echo \"copy select value from spade_query_symbols where name=\$intersect to stdout;\""
+  local z=$(echo "\"copy select value from spade_query_symbols where name=\$intersect to stdout;\"")
   echo "$z"
   local intersect_table_name="echo \"copy select value from spade_query_symbols where name=\$intersect to stdout;\" | ${cli}"
   local v_intersect_size="echo \"copy select count(*) from ${intersect_table_name}_vertex to stdout;\" | ${cli}"
